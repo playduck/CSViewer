8807297bc5e4e45a66d8f75100cb85ce078cc752
playduck <robin.prillwitz@gmail.com>
2020-03-25 12:46:09 +0100

    added list abstractions

---
+++ ListItem.py
@@ -0,0 +1,306 @@
+# ListItem.py
+# by Robin Prillwitz
+# 16.3.2020
+#
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+import pyqtgraph as pg
+import numpy as np
+import pandas
+import Config
+import Graph
+
+class ListItem(QtCore.QObject):
+    sigUpdateUI = QtCore.pyqtSignal()
+    sigCalc = QtCore.pyqtSignal()
+
+    sigDeleteMe = QtCore.pyqtSignal(["QObject"])
+
+    def __init__(self, color, config=None, parent=None):
+        super().__init__()
+
+        self.parent = parent
+
+        if config:
+            self.config = config
+        else:
+            self.config = {
+                "highlight": False,
+                "enabled": True,
+                "zIndex": 0,
+                "xOffset": 0,
+                "yOffset": 0,
+                "xColumn": -1,
+                "yColumn": -1,
+                "color": color,
+                "width": 3,
+                "interpolation": "Linear",
+                "interpolationAmount":  100,
+                "integrate": 0,
+                "filter": 0
+            }
+
+        self.cursor = None
+        self.plot = None
+        self.modData = None
+        self.ignore = False
+
+        self.plot = Graph.Graph(symbol='o', symbolSize=5)
+        self.plot.sigPositionDelta.connect(self.__applyDelta)
+        self.cursor = pg.InfiniteLine(angle=0, movable=False)
+
+        self.item = QtWidgets.QListWidgetItem()
+        self.frame = QtWidgets.QWidget()
+
+        self.frame.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.frame.customContextMenuRequested.connect(self.__contextMenuEvent)
+
+
+    def __copy__(self):
+        raise NotImplementedError
+
+# ---------------------------------- private --------------------------------- #
+
+    def __applyDelta(self, dx, dy):
+        self.config["xOffset"] += dx
+        self.config["yOffset"] += dy
+
+        self.recalculate()
+        self.updateUI()
+        self.updatePlot()
+
+    def __showListItem(self):
+        raise NotImplementedError
+
+    def __contextMenuEvent(self, event):
+        menu = QtGui.QMenu(self.frame)
+        menu.addAction(QtGui.QAction('Exportieren', self))
+        menu.addSeparator()
+        menu.addAction(QtGui.QAction('Löschen', self))
+
+        action = menu.exec_(self.frame.mapToGlobal(event))
+
+        if action:
+            if action.text() == "Exportieren":
+                self.__export()
+            elif action.text() == "Löschen":
+                self.sigDeleteMe.emit(self)
+
+    def __export(self):
+        options = QtWidgets.QFileDialog.Options()
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Exportieren", "",
+                                                  "CSV Datei (*.csv);;Alle Dateinen (*)", options=options)
+        if filename:
+            self.modData.to_csv(filename, encoding='utf-8', index=False)
+
+# ----------------------------------- local ---------------------------------- #
+
+    def showError(self, title, message):
+        error_dialog = QtWidgets.QMessageBox()
+
+        with open(Config.getResource("assets/style.qss"), "r") as fh:
+            error_dialog.setStyleSheet(fh.read())
+
+        error_dialog.setIcon(QtWidgets.QMessageBox.Warning)
+        error_dialog.setWindowTitle("Error")
+        error_dialog.setText(title)
+        error_dialog.setInformativeText(message)
+        error_dialog.setStandardButtons(QtWidgets.QMessageBox.Ok)
+
+        for button in error_dialog.buttons():
+            button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+        error_dialog.exec_()
+
+    def recalculate(self):
+        raise NotImplementedError
+
+    def updateUI(self):
+        raise NotImplementedError
+
+    def updatePlot(self):
+
+        # Calculate all colors
+        color = QtGui.QColor()
+        color.setHsvF(  self.config["color"][0] / 360,
+                        self.config["color"][1] / 100,
+                        self.config["color"][2] / 100)
+        pen = pg.mkPen(color=color, width=self.config["width"])
+
+        highlightColor = QtGui.QColor()
+        highlightColor.setHsvF( self.config["color"][0] / 360,
+                                self.config["color"][1] / 100,
+                                self.config["color"][2] / 100, 0.2)
+        highlightPen = pg.mkPen(color=highlightColor, width=self.config["width"] * 4 + 10)
+
+        # hide if not enabled
+        if not self.config["enabled"]:
+            self.cursor.setPen(pg.mkPen(color=(0, 0, 0, 0), width=0))
+            self.plot.setData(pen=None, symbolPen=None, symbolBrush=None, shadowPen=None)
+
+        else:
+            # set cursors
+            self.cursor.setPen(pg.mkPen(color=color, width=max(1, self.config["width"] / 2)))
+            self.cursor.setZValue(self.config["zIndex"])
+
+            # set to appropritae interpolation with respect to highlighting
+            if self.config["interpolation"] == "Keine":
+                if self.config["highlight"]:
+                    self.plot.setData(pen=None, symbolPen=pen, symbolBrush=highlightColor, shadowPen=None)
+                else:
+                    self.plot.setData(pen=None, symbolPen=pen, symbolBrush=color, shadowPen=None)
+
+            else:
+                self.plot.setData(pen=pen, symbolPen=None, symbolBrush=None, shadowPen=None)
+
+                if self.config["highlight"]:
+                    self.plot.setData(shadowPen=highlightPen)
+
+            # set respective z indecies
+            if self.config["highlight"]:
+                self.cursor.setZValue(Config.Z_IDX_TOP)
+                self.plot.setZValue(Config.Z_IDX_TOP)
+            else:
+                self.cursor.setZValue(self.config["zIndex"])
+                self.plot.setZValue(self.config["zIndex"])
+
+        self.plot.setData(np.array(self.modData["x"]), np.array(self.modData["y"]))
+
+    def update(self):
+        self.recalculate()
+        self.updatePlot()
+        self.updateUI()
+
+    def applyChange(self, evt, who):
+        if who == "color":
+            color = QtGui.QColor()
+            color.setHsvF(self.config["color"][0] / 360, self.config["color"][1] / 100, self.config["color"][2] / 100)
+
+            colorPicker = QtWidgets.QColorDialog()
+
+            newColor = colorPicker.getColor(color).getHsvF()
+            colorPicker.close()
+
+            self.config["color"][0] = int(newColor[0] * 360)
+            self.config["color"][1] = int(newColor[1] * 100)
+            self.config["color"][2] = int(newColor[2] * 100)
+
+            self.settings.findChild(QtWidgets.QPushButton, "color_select_button").setStyleSheet(
+                "background-color: hsv({:d},{:d}%,{:d}%); color: black;".format(
+                    self.config["color"][0], self.config["color"][1], self.config["color"][2]))
+            self.frame.findChild(QtWidgets.QCheckBox, "enable_checkbox").setStyleSheet(
+                "background-color: hsv({:d},{:d}%,{:d}%); color: black;".format(
+                    self.config["color"][0], self.config["color"][1], self.config["color"][2]))
+        else:
+            self.config[who] = evt
+
+        self.update()
+
+# -------------------------------- recoursive -------------------------------- #
+
+    def deselect(self):
+        if self.config["highlight"] == True:
+            self.setHighlight(False)
+            self.updatePlot()
+
+    def getSelected(self):
+        raise NotImplementedError
+
+    def getCount(self, i):
+        # print("DATA", i+1)
+        return i+1
+
+    def deleteSelected(self, plot, selected):
+        raise NotImplementedError
+
+    def updateCursor(self, mousePoint):
+        infoText = ""
+        if self.config.get("enabled"):
+            # find nearest x-sample to mouse-x pos
+            index = np.clip(
+                np.searchsorted(self.modData["x"],
+                [mousePoint.x()])[0],
+                0, len(self.modData["y"]) - 1
+            )
+
+            self.cursor.setPos(self.modData["y"][index])
+
+            infoText = "\t  <span style='color: hsv({:d},{:d}%,{:d}%);'>y={:5.3f}</span>".format(
+                self.config["color"][0],
+                self.config["color"][1],
+                self.config["color"][2],
+                self.modData["y"][index])
+
+        return infoText
+
+    def autoscale(self):
+        if self.config["enabled"]:
+            return [self.plot]
+        else:
+            return []
+
+    def setHighlight(self, highlight):
+        self.config["highlight"] = highlight
+        self.plot.setDraggable(highlight)
+        self.updatePlot()
+
+    def setEnabled(self, enabled):
+        self.config["enabled"] = enabled
+        self.updatePlot()
+
+    def setZIndex(self, zIndex):
+        raise NotImplementedError
+
+
+# modified class from user Spencer at
+# https://stackoverflow.com/questions/20922836/increases-decreases-qspinbox-value-when-click-drag-mouse-python-pyside
+class SuperSpinner(QtWidgets.QLineEdit):
+    valueChanged = QtCore.pyqtSignal("float")
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setValidator(QtGui.QDoubleValidator(parent=self))
+        self.setMaxLength(5)
+        self.setReadOnly(True)
+        self.textChanged.connect(lambda t: self.valueChanged.emit(float(t)))
+        self.setCursor(QtCore.Qt.SizeHorCursor)
+
+        self.mouseStartPos = False
+        self.startValue = 0
+
+        self.min = self.max = 0
+
+    def setRange(self, min, max):
+        self.min = min
+        self.max = max
+
+    def setValue(self, val):
+        self.setText(str(val))
+
+    def mouseDoubleClickEvent(self, e):
+        self.setValue(0)
+
+    def mousePressEvent(self, e):
+        super().mousePressEvent(e)
+        self.mouseStartPos = e.pos().x()
+        self.startValue = float(self.text())
+
+    def mouseMoveEvent(self, e):
+        if self.mouseStartPos:
+
+            if e.modifiers() == QtCore.Qt.ShiftModifier:
+                multiplier = 1
+            elif e.modifiers() == QtCore.Qt.ControlModifier:
+                multiplier = 0.01
+            else:
+                multiplier = 0.1
+
+            valueOffset = round( (self.mouseStartPos - e.pos().x()) * multiplier, 3)
+            value = self.startValue + valueOffset
+            value = min((self.max, max((self.min, value))))
+            self.setValue(value)
+
+    def mouseReleaseEvent(self, e):
+        super().mouseReleaseEvent(e)
+        self.mouseStartPos = False
+        # self.unsetCursor()
---
+++ ListWidget.py
@@ -0,0 +1,148 @@
+# ListItem.py
+# by Robin Prillwitz
+# 16.3.2020
+#
+
+import Process
+from PyQt5 import QtGui, QtCore, QtWidgets
+
+# Custom List to Deselect Items when whitespace is clicked on
+class DeselectableListWidget(QtWidgets.QListWidget):
+    sigSource = QtCore.pyqtSignal(["QListWidget"])
+    sigDest = QtCore.pyqtSignal(["QListWidget"])
+    sigTrigger = QtCore.pyqtSignal()
+    sigDeselect = QtCore.pyqtSignal()
+
+    sigAddFile = QtCore.pyqtSignal("QString", "QListWidget")
+
+    sigUpdateUI = QtCore.pyqtSignal()
+    sigCalc = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__()
+        self.parent = parent
+
+        self.list = []
+
+        self.setVerticalScrollMode(QtWidgets.QAbstractItemView.ScrollPerPixel)
+
+        self.setAcceptDrops(True)
+        self.setDragDropMode(QtWidgets.QAbstractItemView.DragDrop)
+        self.setDefaultDropAction(QtCore.Qt.MoveAction)
+        self.itemClicked.connect(self.activateSelected)
+
+    def addItem(self, li):
+        li.sigUpdateUI.connect(self.sigUpdateUI.emit)
+        li.sigCalc.connect(self.sigCalc.emit)
+
+        self.list.append(li)
+        super().addItem(li.item)
+        super().setItemWidget(li.item, li.frame)
+
+        self.sigUpdateUI.emit()
+        self.sigCalc.emit()
+
+    def deleteSelected(self, plot, selected=None):
+        # get selected item of this list
+        if selected == None:
+            selected = self.selectedItems()
+            if selected:
+                selected = selected[0]
+            else:
+                selected = None
+
+        for index, item in enumerate(self.list):
+            # try deleting every sub item
+            if not item.deleteSelected(plot, selected):
+                # if the item was deleted, remove own references to it
+                self.takeItem(index)
+                self.list.pop(index)
+
+                self.sigUpdateUI.emit()
+                self.sigCalc.emit()
+                return
+
+    def deleteChildren(self, plot):
+        for item in self.list:
+            item.deleteSelected(plot, item.item)
+
+        self.sigUpdateUI.emit()
+        self.sigCalc.emit()
+
+    def activateSelected(self, current):
+        for item in self.list:
+            if item.item == current:
+                item.setHighlight(True)
+                item.updatePlot()
+                return
+
+    def deselectAll(self):
+        self.clearSelection()
+
+        for item in self.list:
+            item.deselect()
+
+    def getSelected(self):
+        selected = self.selectedItems()
+        if selected:
+            return selected[0]
+
+        for item in self.list:
+            selected = item.getSelected()
+
+        return selected
+
+    def getCount(self, i=0):
+        for index, item in enumerate(self.list):
+            i = item.getCount(i)
+        return i
+
+    def setZIndex(self, index):
+        for i in range(0, self.count()):
+            for j in range(0, len(self.list)):
+                if self.item(i) == self.list[j].item:
+                    index = self.list[j].setZIndex(index)
+
+        self.list.sort(key=lambda item: item.config["zIndex"], reverse=True)
+        return index
+
+# ------------------------------- event handler ------------------------------ #
+
+    def mousePressEvent(self, event):
+        self.sigDeselect.emit()
+        QtWidgets.QListWidget.mousePressEvent(self, event)
+
+    def startDrag(self, event):
+        self.sigSource.emit(self)
+        super().startDrag(event)
+        self.sigTrigger.emit()
+        self.sigCalc.emit()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+
+            for url in event.mimeData().urls():
+                self.sigAddFile.emit(str(url.toLocalFile()), self)
+
+        elif event.mimeData().hasText() or event.mimeData().hasHtml():
+            event.ignore()
+        else:
+            super().dropEvent(event)
+            self.sigDest.emit(self)
+
+    def dragHandler(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+        elif event.mimeData().hasText() or event.mimeData().hasHtml():
+            event.ignore()
+        else:
+            event.accept()
+
+    def dragEnterEvent(self, event):
+        self.dragHandler(event)
+
+    def dragMoveEvent(self, event):
+        self.dragHandler(event)
